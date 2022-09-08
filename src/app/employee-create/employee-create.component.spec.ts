2a1c1e1e9a565626116dd303f8a4a8f8f2556d13
Manikandan <26763367+mani2020@users.noreply.github.com>
2022-09-08 16:54:11 +0530

    Add files via upload

--- src/app/employee-create/employee-create.component.spec.ts
+++ src/app/employee-create/employee-create.component.spec.ts
@@ -1,14 +1,21 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RestApiService } from '../shared/rest-api.service';
 
 import { EmployeeCreateComponent } from './employee-create.component';
 
 describe('EmployeeCreateComponent', () => {
   let component: EmployeeCreateComponent;
   let fixture: ComponentFixture<EmployeeCreateComponent>;
+  let restApi: RestApiService;
 
+  const mockRouter = {navigate:jasmine.createSpy("navigate")};
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EmployeeCreateComponent ]
+      declarations: [ EmployeeCreateComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [{provide: Router, useValue:mockRouter}]
     })
     .compileComponents();
   });
@@ -16,10 +23,18 @@ describe('EmployeeCreateComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeCreateComponent);
     component = fixture.componentInstance;
+    restApi = TestBed.inject(RestApiService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call addEmployee', () => {
+    spyOn(restApi,"createEmployee").and.returnValue();
+    component.addEmployee();
+    expect(component).toBeTruthy();
+  });
+
 });
--- src/app/employee-list/employee-list.component.spec.ts
+++ src/app/employee-list/employee-list.component.spec.ts
@@ -1,14 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { EmployeeListComponent } from './employee-list.component';
+import { RestApiService } from '../shared/rest-api.service';
+import { Employee } from '../shared/employee';
+
 
 describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
   let fixture: ComponentFixture<EmployeeListComponent>;
-
+  let restApi: RestApiService;
+  let users: Employee[] = [{
+    "id": "2",
+    "name": "Black Widowasdf",
+    "email": "black-widow@mcu.com",
+    "phone": 9798869898
+  },
+  {
+    "name": "Mani",
+    "email": "mani@gmail.com",
+    "phone": 98798798788,
+    "id": "5"
+  }];
+  let remainingUsers: Employee[] = [
+  {
+    "id": "2",
+    "name": "Black Widowasdf",
+    "email": "black-widow@mcu.com",
+    "phone": 9798869898
+  }];  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EmployeeListComponent ]
+      declarations: [ EmployeeListComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
@@ -16,10 +39,26 @@ describe('EmployeeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeListComponent);
     component = fixture.componentInstance;
+    restApi = TestBed.inject(RestApiService);
+    
+    //spyOn(restApi,"deleteEmployee").withArgs("2").and.returnValue();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call loadEmployees', () => {
+    spyOn(restApi,"getEmployees").and.returnValue(users);
+    component.loadEmployees();
+    expect(component.Employee).toEqual(users);
+  });
+  
+  it('should call deleteEmployee', () => {
+    spyOn(restApi,"getEmployees").and.returnValue(remainingUsers);
+    component.deleteEmployee("5");
+    expect(component.Employee).toEqual(remainingUsers);
+  });  
+
 });
